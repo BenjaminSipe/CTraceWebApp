0596c7e1fdbf2c18b1240813181f5f37eec41327
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2021-03-22 20:09:20 -0500

    Added Contact form and settled on picker.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Main, Heading, Paragraph, TextInput } from "grommet"
+import CaseForm from './CaseForm'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Main pad="large">
+          <Heading>Something</Heading>
+          <Paragraph>Something about something</Paragraph>
+          <CaseForm/>
+
+        </Main>
       </header>
     </div>
   );
 }
+
+// {
+//   "name": "Eli Tobin",
+//   "address": "100 Opportunity Ave. Point Lookout Mo, 65726",
+//   "dob": "02/17/1999",
+//   "quarantineLocation": "On Campus",
+//   "email": "213312@student.cofo.edu",
+//   "phone": "(417) 337-2836",
+//   "doc": "2/3/2021"
+// }
+
 
 export default App;
