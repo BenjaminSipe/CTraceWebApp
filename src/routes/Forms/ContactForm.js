246aa117de2c0c3e96af9c3fbd91fe178cf1e4c3
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2021-03-29 21:50:47 -0500

    Added redirect from contact on submit

--- src/routes/Forms/ContactForm.js
+++ src/routes/Forms/ContactForm.js
@@ -11,7 +11,7 @@ import {
   Button,
   Heading,
 } from "grommet";
-
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import {
   dateMask,
@@ -24,6 +24,7 @@ class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      redirect: false,
       value: {
         name: props.query.get("name") || "",
         address: props.query.get("address") || "",
@@ -31,114 +32,128 @@ class ContactForm extends Component {
         dob: props.query.get("dob") || "",
         doc: props.query.get("doc") || "",
         phone: props.query.get("phone") || "",
+        quarantineLocation: "",
       },
     };
   }
+  // this also works with react-router-native
+
   render() {
     const { value } = this.state;
-    return (
-      <Grommet>
-        <Heading alignSelf="start">C-Trace</Heading>
-        <h2>Close Contact Form</h2>
-        <Form
-          value={value}
-          onChange={(nextValue) => {
-            this.setState({ value: nextValue });
-          }}
-          onReset={() => this.setState({ value: {} })}
-          onSubmit={({ value }) => {
-            axios
-              .post("http://localhost:3000/api/contact", value)
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-          }}
-        >
-          <FormField name="name" htmlFor="textinput-name" label="Full Name">
-            <MaskedInput
-              required
-              id="textinput-name"
-              name="name"
-              mask={nameMask()}
-              value={value.name}
-            />
-          </FormField>
-          <FormField name="address" htmlFor="textinput-address" label="Address">
-            <TextInput
-              required
-              id="textinput-address"
+    if (this.state.redirect) {
+      return <Redirect to="/postform" />;
+    } else {
+      return (
+        <Grommet>
+          <Heading alignSelf="start">C-Trace</Heading>
+          <h2>Close Contact Form</h2>
+          <Form
+            value={value}
+            onChange={(nextValue) => {
+              this.setState({ value: nextValue });
+            }}
+            onReset={() => this.setState({ value: {} })}
+            onSubmit={({ value }) => {
+              axios
+                .post("http://localhost:3000/api/contact", value)
+                .then(function (response) {
+                  console.log(response);
+                })
+                .catch(function (error) {
+                  console.log(error);
+                })
+                .then(() => {
+                  this.setState({ redirect: true });
+                });
+            }}
+          >
+            <FormField name="name" htmlFor="textinput-name" label="Full Name">
+              <MaskedInput
+                required
+                id="textinput-name"
+                name="name"
+                mask={nameMask()}
+                value={value.name}
+              />
+            </FormField>
+            <FormField
               name="address"
-              placeholder="1 MyStreet Road, MyTown MO 00000"
-            />
-          </FormField>
-          <FormField
-            name="quarantineLocation"
-            htmlFor="textinput-address"
-            label="quarantineLocation"
-          >
-            <TextInput
-              required
-              id="textinput-address"
+              htmlFor="textinput-address"
+              label="Address"
+            >
+              <TextInput
+                required
+                id="textinput-address"
+                name="address"
+                placeholder="1 MyStreet Road, MyTown MO 00000"
+              />
+            </FormField>
+            <FormField
               name="quarantineLocation"
-              placeholder="1 MyStreet Road, MyTown MO 00000"
-            />
-          </FormField>
-          <FormField name="email" htmlFor="textinput-email" label="email">
-            <MaskedInput
-              required
-              id="textinput-email"
-              name="email"
-              placeholder="example@email.com"
-              mask={emailMask()}
-              value={value.email}
-            />
-          </FormField>
-          <FormField name="dob" htmlFor="textinput-dob" label="Date of Birth">
-            <MaskedInput
-              required
-              id="textinput-dob"
-              name="dob"
-              mask={dateMask(value.dob)}
-              value={value.dob}
-              // onKeyUp={(e) => console.log(e.keyCode)}
-            />
-          </FormField>
-          <FormField
-            name="doc"
-            htmlFor="textinput-doc"
-            label="Date of Close Contact"
-          >
-            <MaskedInput
-              required
-              id="textinput-doc"
+              htmlFor="textinput-address"
+              label="quarantineLocation"
+            >
+              <TextInput
+                required
+                id="textinput-address"
+                name="quarantineLocation"
+                placeholder="1 MyStreet Road, MyTown MO 00000"
+              />
+            </FormField>
+            <FormField name="email" htmlFor="textinput-email" label="email">
+              <MaskedInput
+                required
+                id="textinput-email"
+                name="email"
+                placeholder="example@email.com"
+                mask={emailMask()}
+                value={value.email}
+              />
+            </FormField>
+            <FormField name="dob" htmlFor="textinput-dob" label="Date of Birth">
+              <MaskedInput
+                required
+                id="textinput-dob"
+                name="dob"
+                mask={dateMask(value.dob)}
+                value={value.dob}
+                // onKeyUp={(e) => console.log(e.keyCode)}
+              />
+            </FormField>
+            <FormField
               name="doc"
-              mask={dateMask(value.doc)}
-              value={value.doc}
-            />
-          </FormField>
-          <FormField
-            name="phone"
-            htmlFor="textinput-phone"
-            label="Phone Number"
-          >
-            <MaskedInput
-              required
-              id="textinput-phone"
+              htmlFor="textinput-doc"
+              label="Date of Close Contact"
+            >
+              <MaskedInput
+                required
+                id="textinput-doc"
+                name="doc"
+                mask={dateMask(value.doc)}
+                value={value.doc}
+              />
+            </FormField>
+            <FormField
               name="phone"
-              mask={phoneNumberMask()}
-              value={value.phone}
-            />
-          </FormField>
-          <Box direction="row" gap="medium">
-            <Button type="submit" primary label="Submit" />
-            <Button type="reset" label="Reset" />
-          </Box>
-        </Form>
-      </Grommet>
-    );
+              htmlFor="textinput-phone"
+              label="Phone Number"
+            >
+              <MaskedInput
+                required
+                id="textinput-phone"
+                name="phone"
+                mask={phoneNumberMask()}
+                value={value.phone}
+              />
+            </FormField>
+            <Box direction="row" gap="medium">
+              <Button type="submit" primary label="Submit" />
+              <Button type="reset" label="Reset" />
+            </Box>
+          </Form>
+        </Grommet>
+      );
+    }
   }
 }
 
